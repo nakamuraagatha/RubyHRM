eba7b66bf641fd7874fd346277bf9ea43b999eca
Robert Buckley <buckleyrobert@gmail.com>
2015-07-16 20:30:55 -0400

    adding precompiled assets for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,10 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  config.serve_static_assets = true
+
+  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
