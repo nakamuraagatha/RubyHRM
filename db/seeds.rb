6a3ea0d79711cde07a279c3134c1454cc55aeb03
Robert Buckley <buckleyrobert@gmail.com>
2015-07-23 21:20:44 -0400

    Add translation seeds to seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create(username: 'Administrator', password: 'password', password_confirmation: 'password', role: 'admin')
+
+Translation.create(locale: 'en', key: 'login.username', value: 'Username')
+Translation.create(locale: 'en', key: 'login.password', value: 'Password')
+Translation.create(locale: 'en', key: 'login.header', value: 'Sign in')
+Translation.create(locale: 'en', key: 'login.login', value: 'Login')
+Translation.create(locale: 'en', key: 'login.forgotpassword', value: 'Forgot password?')
+Translation.create(locale: 'en', key: 'login.rememberme', value: 'Remember me?')
