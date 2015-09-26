f69ae57a75013e1d6d25b2309042d94173fdded4
Robert Buckley <buckleyrobert@gmail.com>
2015-09-26 11:13:22 -0400

    Fixed small issues with saving location, viewing job history when location is saved, and deleting job history

--- app/controllers/job_detail_histories_controller.rb
+++ app/controllers/job_detail_histories_controller.rb
@@ -5,7 +5,7 @@ class JobDetailHistoriesController < ApplicationController
 		@job_detail_history = JobDetailHistory.find(params[:id])
 		if @job_detail_history.destroy
 			flash[:success] = "Job history has been deleted"
-			redirect_to job_details_path(@job_detail_history.person_id)
+			redirect_to person_job_detail_path(@job_detail_history.job_detail_id, @job_detail_history.job_detail_id)
 		end
 	end
 end
